14b4ab8dd7754795976aeb9b8aadde1b3ade8ccf
Sidra <sidra.maqbool@pioneeringprogrammers.com>
2023-03-01 19:18:38 +0500

    example of mutating

--- filter/src/App.js
+++ filter/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, name: "item 1" },
+    { id: 2, name: "item 2" },
+    { id: 3, name: "item 3" },
+  ]);
+
+  const handleDeleteItem = (id) => {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {items.map((item) => (
+        <div key={item.id}>
+          <span>{item.name}</span>
+          <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+        </div>
+      ))}
     </div>
   );
 }
