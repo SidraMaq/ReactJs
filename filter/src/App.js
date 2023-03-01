cf71ec6496ec01a30388b98e624fa77989beb91a
Sidra <sidra.maqbool@pioneeringprogrammers.com>
2023-03-01 19:20:38 +0500

    update app component

--- filter/src/App.js
+++ filter/src/App.js
@@ -24,3 +24,12 @@ function App() {
 }
 
 export default App;
+
+/**
+ * In this example, the handleDeleteItem function uses the filter() method
+ *  to filter out the item with the matching id and then updates the items state using
+ *  the setItems function. The setItems function is called with a function that 
+ * takes the previous state (prevItems) as an argument and returns a new state with the item
+ *  removed, rather than directly mutating the state. 
+ * This ensures that the state is updated correctly and avoids any unexpected behavior.
+ */
