f817b699b0e775c113ec753321f35ac66cbeff48
Sidra <sidra.maqbool@pioneeringprogrammers.com>
2023-03-05 21:17:37 +0500

    simple counter application

---
+++ counter-app/src/counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+function Counter() {
+  //initialize a state variable
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
