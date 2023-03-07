f69ca755cf79508cb34134ec7e77511d465e7a6b
Sidra <sidra.maqbool@pioneeringprogrammers.com>
2023-03-07 12:26:54 +0500

    add stopwatch project

---
+++ stopwatch/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Stopwatch from "./stopwatch";
+
+function App() {
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
---
+++ stopwatch/src/stopwatch.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+
+function Stopwatch() {
+  //declares a state variable isRunning and sets its initial value to false
+  //declares a function named setIsRunning that can be update the value of isRunning
+  const [isRunning, setIsRunning] = useState(false);
+  //declares a state variable elapsedTime and sets initial value 0
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+    //when "isRunning" is true
+    if (isRunning) {
+      //setInterval function is called with a callback function
+      intervalId = setInterval(() => {
+        //updates the "elapsedTime" state variable every 10 milliseconds by adding 10 to its previous value
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 10);
+      }, 10);
+    }
+    // setInterval function returns an ID that is saved in the "intervalId" variable.
+    return () => clearInterval(intervalId);
+    //The useEffect hook is triggered whenever the value of "isRunning" changes,
+    //which ensures that the timer starts and stops based on the value of "isRunning".
+  }, [isRunning]);
+
+  //this function toggles the value of the isRunning state variable by using
+  //setIsRunning with the help of prevIsRunning parameter passed to it
+  const handleStartStopClick = () => {
+    //if current value of isRunning false it will update to true
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+  //this function sets the value of both "isRunning" and "elapsedTime" state variables to their initial values.
+  const handleResetClick = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  };
+  //this function takes time in milliseconds as input and return
+  //a formated time MM:SS.mm
+  const formatTime = (timeInMillis) => {
+    //Calculates the number of minuts based on input
+    const minuts = Math.floor(timeInMillis / 60000);
+    //calculates the number of seconds based on input
+    const seconds = Math.floor((timeInMillis % 60000) / 1000);
+    //calculates the number of miliseconds based on input
+    const milliseconds = Math.floor((timeInMillis % 1000) / 10);
+    //converts these values into strings and pads them with leading zeros
+    //if necessary using the "padStart" method.
+    return `${minuts.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}.${milliseconds.toString().padStart(2, "0")}`;
+    //function combines the minutes, seconds, and milliseconds values into a formatted string and returns it.
+  };
+
+  return (
+    <>
+      <h1>Stopwatch</h1>
+      <p>
+        Build a stopwatch application using the useState and useEffect hooks,
+        which displays the elapsed time when a user clicks start and stop
+      </p>
+      <p>{formatTime(elapsedTime)}</p>
+      <button onClick={handleStartStopClick}>
+        {isRunning ? "Stop" : "Start"}
+      </button>
+      <button onClick={handleResetClick}>Reset</button>
+    </>
+  );
+}
+export default Stopwatch;
